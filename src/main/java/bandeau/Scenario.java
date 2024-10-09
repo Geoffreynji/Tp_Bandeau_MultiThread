d70a77d0fa53cc99081917f8f70852e905da6b9c
Geoffrey Naji <geoffrey.naji@etud.univ-jfc.fr>
2024-10-09 09:59:05 +0200

    Verrous read-write implémenté

--- src/main/java/bandeau/Scenario.java
+++ src/main/java/bandeau/Scenario.java
@@ -1,6 +1,8 @@
 package bandeau;
 import java.util.List;
 import java.util.LinkedList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Classe utilitaire pour représenter la classe-association UML
@@ -22,6 +24,9 @@ class ScenarioElement {
 public class Scenario {
 
     private final List<ScenarioElement> myElements = new LinkedList<>();
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Lock lecture = lock.readLock();
+    private final Lock ecriture = lock.writeLock();
 
     /**
      * Ajouter un effect au scenario.
@@ -29,20 +34,51 @@ public class Scenario {
      * @param e l'effet à ajouter
      * @param repeats le nombre de répétitions pour cet effet
      */
-    public void addEffect(Effect e, int repeats) {
+
+    private class ThreadWithScenario extends Thread
+    {
+        private Bandeau newBandeau;
+
+        public ThreadWithScenario(Bandeau newBandeau) {
+            this.newBandeau = newBandeau;
+        }
+
+        public void run() {
+            lecture.lock();  //Utilise le verrou de lecture pour garantir que aucun thread ne modifie
+            try {
+                for (ScenarioElement element : myElements) {
+                    for (int repeats = 0; repeats < element.repeats; repeats++) {
+                        element.effect.playOn(newBandeau);
+                    }
+                }
+            } finally {
+                lecture.unlock();  // Déverrouiller pour éviter les blocages
+            }
+        }
+}
+
+
+
+public void addEffect(Effect e, int repeats) {
+    ecriture.lock();  
+    try {
         myElements.add(new ScenarioElement(e, repeats));
+    } finally {
+        ecriture.unlock();  // Déverrouille après la modification
     }
+}
+
 
     /**
      * Jouer ce scenario sur un bandeau
      *
      * @param b le bandeau ou s'afficher.
      */
-    public void playOn(Bandeau b) {
-        for (ScenarioElement element : myElements) {
-            for (int repeats = 0; repeats < element.repeats; repeats++) {
-                element.effect.playOn(b);
-            }
-        }
-    }
+    public void playOn(Bandeau newBandeau) {  
+        ThreadWithScenario Bandeau = new ThreadWithScenario(newBandeau);
+        Bandeau.start();
+
 }
+
+}
+
